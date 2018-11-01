5ddd07ac136d300cf8fc85e46c41d9a73a245464
Sami Eriksson <sami.eriksson@yahoo.fi>
2018-11-01 16:45:19 +0200

    Toiminnallisuutta, kesken...

--- toiminnallisuudet.js
+++ toiminnallisuudet.js
@@ -1,4 +1,4 @@
-
+var viestiketjuid;
 // Haetaan kaikki viestit, ja tehdään niistä taulukko.
 $(document).ready(function() {
     $.ajax({
@@ -35,62 +35,126 @@ function myFunction(id) {
     // } else {
     //     x.style.display = "none";
     // }
-
+    viestiketjuid=id;
+    console.log("myF");
     $.ajax({
         url: "http://localhost:8080/idt/" + id
     }).then(function(data) {
         console.dir(data);
+        var $aiherivi = $('#' + id);
+        var $vastauslomake = $('#vastauslomake');
 
-        for(var viesti = 0; viesti <= data.length; viesti++) {
+        var pastekohde = $aiherivi;
+
+        // etsitään viestiketjuun liittyvät vastausviestit
+        for(var viesti = 0; viesti < data.length; viesti++) {
             var teksti = data[viesti].text;
             var nimi = data[viesti].name;
 
-            console.log(teksti);
-            console.log(nimi);
-
-            var $aiherivi = $('#' + id);
-            var $taulukonrivit = $('#taulukonrivit');
-            var $tr = $('<tr></tr>');
+            // var $taulukonrivit = $('#taulukonrivit');
+            var $tr = $('<tr></tr>').attr('id', 'kohde' + viesti);
 
             $tr.append($('<td></td>').text(teksti));
             $tr.append($('<td></td>').text(nimi));
             // $aiherivi.append($tr);
             // $aiherivi.insertAfter($tr);
-            $aiherivi.after($tr);
+            if(viesti > 0) {
+                $aiherivi.after($tr);
+            } else {
+                $('#kohde' + (viesti-1)).after($tr);
+            }
+            //pastekohde = viimeisin;
 
+            $aiherivi.after($vastauslomake);
         }
+
+        // TONIN KOODI:
+       //  var $trlomake = $('<tr></tr>');
+       //
+       //  $trlomake.append($('<td></td>').append($vastauslomake));
+       //
+       // $aiherivi.after($trlomake);
+       // console.dir($trlomake);
+       // <--- TONIN KOODI
+       //-----------------------------------------------------
+       /* $tr.append($('<td></td>').text(nimi));
+        var lomake = $('<form></form>').attr('method', 'post');
+        var tekstiboksi = $('<input></input>').attr({
+            type:"text",
+            id:"tekstiboksi"
+        });
+        var nimiboksi = $('<input></input>').attr({
+            type:"text",
+            id:"nimiboksi"
+        });
+        var submitbuttoni = $('<button></button>').attr({
+            type:"submit",
+            id:"submitbuttoni"
+        });
+
+        lomake.append(tekstiboksi);
+        lomake.append(nimiboksi);
+        lomake.append(submitbuttoni);
+
+        var $uusitr = $('<tr></tr>');
+        $uusitr.append(lomake);
+        $aiherivi.after($uusitr);*/
+
+
+
+
+        // $('#vastauslomake').appendTo($aiherivi);
 
 
     });
 }
-
+function kommentoi(){
+    console.log("Kommentoi...");
+    console.log(viestiketjuid);
+}
 // Luodaan uusi aihe
 $("#lomake").submit(function(e){
 // $("#testibuttoni").on("click", function(e){ <<---- TESTIHOMMIA
 
-    // e.preventDefault();
-
-    // get values from textboxs
-    var aihe = $('#aiheboksi').val();
-    var teksti = $('#tekstiboksi').val();
-    var nimi = $('#nimiboksi').val();
-
-    var data = {name:nimi, text:teksti, topic:aihe};
-
     $.ajax({
-        headers: {
-            "Accept": "application/json",
-            "Content-Type":"application/json",
-        },
-        url:"http://localhost:8080/",
-        type:"POST",
-        dataType:"json",
-        data: JSON.stringify(data),
-        success: function(response){
-            // alert(JSON.stringify(response));
-        },
-        error: function(err){
-            alert(JSON.stringify(err));
-        }
-    })
+        url: "http://localhost:8080/"
+    }).then(function(kokodata) {
+        console.dir(kokodata);
+        var generoituid = kokodata.length + 1;
+
+        // get values from textboxs
+        var aihe = $('#aiheboksi').val();
+        var teksti = $('#tekstiboksi').val();
+        var nimi = $('#nimiboksi').val();
+
+        var data = {name:nimi, text:teksti, topic:aihe, messageId:generoituid};
+
+        $.ajax({
+            headers: {
+                "Accept": "application/json",
+                "Content-Type":"application/json",
+            },
+            url:"http://localhost:8080/",
+            type:"POST",
+            dataType:"json",
+            data: JSON.stringify(data),
+
+
+            // success: function(response){
+            //     // alert(JSON.stringify(response));
+            // },
+            // error: function(err){
+            //     // alert(JSON.stringify(err));
+            // }
+        })
+
+    });
+
 });
+
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+});
+
